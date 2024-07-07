aab9ab0e918786a082aa91c317ff57cf0f4eedea
PannyaTrehan <s4003909@student.rmit.edu.au>
2024-07-07 23:46:50 +1000

    Implemented add a problem page

--- client/src/pages/Add.tsx
+++ client/src/pages/Add.tsx
@@ -1,8 +1,33 @@
+import { Container, Row, Col, Form, InputGroup } from 'react-bootstrap';
+import { FaSearch } from 'react-icons/fa';
+import ProblemsTable from '../components/ProblemsTable';
 
 function Add() {
     return(
-        <>
-        </>
+        <Container>
+            <Col>
+                <Row className='mt-4'>
+                    <h1>Add a problem</h1>
+                </Row>
+                <Row>
+                    <InputGroup className="mb-3">
+                        <InputGroup.Text id="basic-addon2">
+                            <FaSearch />
+                        </InputGroup.Text>
+                        <Form.Control
+                            placeholder="Search for a problem"
+                            aria-label="Search for a problem"
+                            aria-describedby="basic-addon2"
+                        />
+                    </InputGroup>
+                    <ProblemsTable />
+                </Row>
+                <Row className='mt-4'>
+                    <h1>Recently completed</h1>
+                    <ProblemsTable />
+                </Row>
+            </Col>
+        </Container>
     );
 }
 
